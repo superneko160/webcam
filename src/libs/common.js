46941f5c29ee0c3d3da3ccf89bd2def2a1d3468f
superneko <supernekocat31@gmail.com>
2023-06-21 01:03:15 +0900

    Compatible with responsive design

--- src/libs/common.js
+++ src/libs/common.js
@@ -2,13 +2,14 @@ const fs = require('fs');
 
 // 時間取得（年-月-日T時分秒）
 const getNow = () => {
-    let dt = new Date(),
-    y = dt.getFullYear(),
-    m = ('00' + (dt.getMonth()+1)).slice(-2),
-    d = ('00' + dt.getDate()).slice(-2),
-    h = ('00' + dt.getHours()).slice(-2),
-    min = ('00' + dt.getMinutes()).slice(-2),
-    s = ('00' + dt.getSeconds()).slice(-2);
+    let dt = new Date();
+    dt.setTime(dt.getTime() + 9 * 60 * 60 * 1000);  // タイムゾーンを日本時間に調整
+    let y = dt.getFullYear(),
+        m = ('00' + (dt.getMonth()+1)).slice(-2),
+        d = ('00' + dt.getDate()).slice(-2),
+        h = ('00' + dt.getHours()).slice(-2),
+        min = ('00' + dt.getMinutes()).slice(-2),
+        s = ('00' + dt.getSeconds()).slice(-2);
     return `${y}-${m}-${d}T${h}${min}${s}`;
 };
 
