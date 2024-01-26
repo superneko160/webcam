20248ac435cbe18e2396e1948a2c36a8aff35f19
sneko <supernekocat31@gmail.com>
2024-01-27 02:14:43 +0900

    ESLint introduced

--- src/index.js
+++ src/index.js
@@ -1,40 +1,39 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
-const common = require('./libs/common')
+const common = require('./libs/common');
 
-const port = 3000
-const picdir = 'pictures'
+const port = 3000;
+const picdir = 'pictures';
 
-app.use('/', express.static('public'))
+app.use('/', express.static('public'));
 
 // データ取得
 io.on('connection', (socket) => {
-    socket.on('videoimg', (data) => { saveImg(data) })
-})
+  socket.on('videoimg', (data) => { saveImg(data); });
+});
 
 // 画像を保存
 const saveImg = (data) => {
-    const base64 = data.img.split(",")[1]  // imgの中身は0番目に保存形式などの基本情報、1番目に画像のデータがある
-    const decode = new Buffer.from(base64, 'base64')
-    // 画像保存用のディレクトリがなければ作成
-    common.mkDir(picdir)
-    // 年-月-日T時分秒.jpgで画像書込
-    const date = common.getNow()
-    fs.writeFile(`${picdir}/${date}.jpg`, decode, (err) => {
-        // 画像保存の成否をクライアント側に送信
-        if (err) {
-            console.log(err)
-            io.emit('msg', '保存に失敗しました')
-        }
-        else {
-            console.log(`save image: ${date}.jpg`)
-            io.emit('msg', '保存しました')
-        }
-    })
+  const base64 = data.img.split(',')[1]; // imgの中身は0番目に保存形式などの基本情報、1番目に画像のデータがある
+  const decode = Buffer.from(base64, 'base64');
+  // 画像保存用のディレクトリがなければ作成
+  common.mkDir(picdir);
+  // 年-月-日T時分秒.jpgで画像書込
+  const date = common.getNow();
+  fs.writeFile(`${picdir}/${date}.jpg`, decode, (err) => {
+    // 画像保存の成否をクライアント側に送信
+    if (err) {
+      console.log(err);
+      io.emit('msg', '保存に失敗しました');
+    } else {
+      console.log(`save image: ${date}.jpg`);
+      io.emit('msg', '保存しました');
+    }
+  });
 };
 
 // ポート開設
-http.listen(port, () => console.log(`Listening on port: ${port}`))
+http.listen(port, () => console.log(`Listening on port: ${port}`));
--- src/public/js/webcam.js
+++ src/public/js/webcam.js
@@ -1,77 +1,75 @@
 // HTML要素の取得
-const video = document.getElementById("video")
-const canvas = document.getElementById('canvas')
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const savebtn = document.getElementById("savebtn")
+const savebtn = document.getElementById('savebtn');
 
-// socket.io
+// eslint-disable-next-line
 const socket = io();
 
 // 起動処理
 const init = () => {
-    setCamera()
-    setButton()
-    setResultSocket()
+  setCamera();
+  setButton();
+  setResultSocket();
 };
 
 // カメラ処理
 const setCamera = () => {
-    navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: false,
-    }).then(stream => {
-        video.srcObject = stream;
-        video.play()
-        drawCanvas()
-    }).catch(err => {
-        console.log(err)
-        showMessageInCanvas("カメラの接続が許可されていません")
-    })
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false
+  }).then(stream => {
+    video.srcObject = stream;
+    video.play();
+    drawCanvas();
+  }).catch(err => {
+    console.log(err);
+    showMessageInCanvas('カメラの接続が許可されていません');
+  });
 };
 
 //  canvasの上にvideoを描画
 const drawCanvas = () => {
-    const draw = function(){
-        ctx.drawImage(video, 0, 0)
-    }
-    setInterval(draw, 100)
+  const draw = () => {
+    ctx.drawImage(video, 0, 0);
+  };
+  setInterval(draw, 100);
 };
 
 // ボタン処理
 const setButton = () => {
-    // カーソル選択時
-    savebtn.addEventListener("mouseover", () => {
-        savebtn.style.backgroundColor = "#4169e1"
-    })
-    // カーソルがボタンから離れたとき（もとの色に戻す）
-    savebtn.addEventListener("mouseleave", () => {
-        savebtn.style.backgroundColor = "#00a8e1"
-    })
-    // ボタン押下時、canvasの画像をサーバに送信
-    savebtn.addEventListener("click", () => {
-        let data = {
-            "img": canvas.toDataURL("image/jpeg")
-        };
-        socket.emit('videoimg', data)
-    })
+  // カーソル選択時
+  savebtn.addEventListener('mouseover', () => {
+    savebtn.style.backgroundColor = '#4169e1';
+  });
+  // カーソルがボタンから離れたとき（もとの色に戻す）
+  savebtn.addEventListener('mouseleave', () => {
+    savebtn.style.backgroundColor = '#00a8e1';
+  });
+  // ボタン押下時、canvasの画像をサーバに送信
+  savebtn.addEventListener('click', () => {
+    const data = { img: canvas.toDataURL('image/jpeg') };
+    socket.emit('videoimg', data);
+  });
 };
 
 // 画像保存の成否のメッセージを取得
 const setResultSocket = () => {
-    socket.on('msg', (val) => { alert(val) })
+  socket.on('msg', (val) => { alert(val); });
 };
 
 // メッセージをCanvas内に表示
 const showMessageInCanvas = (message) => {
-    const fontSize = 24;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#000"; // 背景色を黒に設定
-    ctx.fillRect(0, 0, canvas.width, canvas.height); 
-    ctx.fillStyle = "#fff";
-    ctx.font = `${fontSize}px sans-serif`;
-    ctx.textAlign = "center";
-    ctx.fillText(message, canvas.width / 2, canvas.height / 2);
+  const fontSize = 24;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#000'; // 背景色を黒に設定
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#fff';
+  ctx.font = `${fontSize}px sans-serif`;
+  ctx.textAlign = 'center';
+  ctx.fillText(message, canvas.width / 2, canvas.height / 2);
 };
 
 // 処理の起動
-init()
+init();
